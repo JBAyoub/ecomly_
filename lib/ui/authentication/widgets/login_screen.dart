dac37edd9da9f626c1c8f42bb3057de3c9b017e3
Ayb <aymojobe@gmail.com>
2026-09-10 14:09:44 +0100

    logic screen overflow

--- lib/ui/authentication/widgets/login_screen.dart
+++ lib/ui/authentication/widgets/login_screen.dart
@@ -36,109 +36,126 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       resizeToAvoidBottomInset: true,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Column(
-            spacing: 10,
-            crossAxisAlignment: .stretch,
-            children: [
-              Text("Login to your account", style: textTheme.headlineLarge),
-              Text(
-                "It's great to see you again.",
-                style: textTheme.bodyLarge?.copyWith(color: AppColors.grey3),
-              ),
-              const SizedBox(height: 20),
-              Form(
-                key: _formKey,
-                autovalidateMode: .onUserInteraction,
+        child: CustomScrollView(
+          slivers: [
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
                 child: Column(
-                  mainAxisSize: .max,
                   spacing: 10,
+                  crossAxisAlignment: .stretch,
                   children: [
-                    CustomFormField(
-                      fieldController: emailController,
-                      hint: 'Enter your email address',
-                      labelText: 'Email',
-                      textInputType: .emailAddress,
-                      onValidityChanged: (isValid) =>
-                          setState(() => _isEmailValid = isValid),
+                    Text(
+                      "Login to your account",
+                      style: textTheme.headlineLarge,
                     ),
-                    CustomFormField(
-                      fieldController: passwordController,
-                      hint: 'Enter your password',
-                      labelText: 'Password',
-                      hideText: true,
-                      isPassword: true,
-                      onValidityChanged: (isValid) =>
-                          setState(() => _isPasswordValid = isValid),
+                    Text(
+                      "It's great to see you again.",
+                      style: textTheme.bodyLarge?.copyWith(
+                        color: AppColors.grey3,
+                      ),
                     ),
+                    const SizedBox(height: 20),
+                    Form(
+                      key: _formKey,
+                      autovalidateMode: .onUserInteraction,
+                      child: Column(
+                        mainAxisSize: .max,
+                        spacing: 10,
+                        children: [
+                          CustomFormField(
+                            fieldController: emailController,
+                            hint: 'Enter your email address',
+                            labelText: 'Email',
+                            textInputType: .emailAddress,
+                            onValidityChanged: (isValid) =>
+                                setState(() => _isEmailValid = isValid),
+                          ),
+                          CustomFormField(
+                            fieldController: passwordController,
+                            hint: 'Enter your password',
+                            labelText: 'Password',
+                            hideText: true,
+                            isPassword: true,
+                            onValidityChanged: (isValid) =>
+                                setState(() => _isPasswordValid = isValid),
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                'Forgot your password?',
+                                style: textTheme.bodyMedium,
+                              ),
+                              TextButton(
+                                style: TextButton.styleFrom(
+                                  overlayColor: Colors.transparent,
+                                ),
+                                onPressed: () {
+                                  Navigator.of(context)
+                                      .pushNamed('/forgot-password');
+                                },
+                                child: Text(
+                                  'Reset your password.',
+                                  style: textTheme.bodyMedium?.copyWith(
+                                    decoration: .underline,
+                                    fontWeight: .w600,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          BlackButtonPrimary(
+                            buttonText: 'Login',
+                            onPressed: _isFormValid ? onLoginClicked : null,
+                          ),
+                          const SizedBox(height: 10),
+                          const OrDivider(),
+                          const SizedBox(height: 10),
+
+                          LoginWithGoogleButton(buttonText: 'Log in'),
+                          const SizedBox(height: 5),
+                          LoginWithFacebookButton(buttonText: 'Log in'),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
                     Row(
+                      mainAxisAlignment: .center,
+                      mainAxisSize: .min,
                       children: [
                         Text(
-                          'Forgot your password?',
-                          style: textTheme.bodyMedium,
+                          "Don't have an account?",
+                          style: textTheme.bodyLarge?.copyWith(
+                            color: AppColors.grey2,
+                          ),
                         ),
                         TextButton(
                           style: TextButton.styleFrom(
                             overlayColor: Colors.transparent,
                           ),
                           onPressed: () {
-                            Navigator.of(context).pushNamed('/forgot-password');
+                            Navigator.of(context)
+                                .pushNamed('/account_registration');
                           },
                           child: Text(
-                            'Reset your password.',
-                            style: textTheme.bodyMedium?.copyWith(
-                              decoration: .underline,
+                            'Join',
+                            style: textTheme.bodyLarge?.copyWith(
                               fontWeight: .w600,
+                              decoration: .underline,
                             ),
                           ),
                         ),
                       ],
                     ),
-                    BlackButtonPrimary(
-                      buttonText: 'Login',
-                      onPressed: _isFormValid ? onLoginClicked : null,
-                    ),
-                    const SizedBox(height: 10),
-                    const OrDivider(),
-                    const SizedBox(height: 10),
-
-                    LoginWithGoogleButton(buttonText: 'Log in'),
-                    const SizedBox(height: 5),
-                    LoginWithFacebookButton(buttonText: 'Log in'),
                   ],
                 ),
               ),
-              const Spacer(),
-              Row(
-                mainAxisAlignment: .center,
-                mainAxisSize: .min,
-                children: [
-                  Text(
-                    "Don't have an account?",
-                    style: textTheme.bodyLarge?.copyWith(
-                      color: AppColors.grey2,
-                    ),
-                  ),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      overlayColor: Colors.transparent,
-                    ),
-                    onPressed: () {
-                      Navigator.of(context).pushNamed('/account_registration');
-                    },
-                    child: Text(
-                      'Join',
-                      style: textTheme.bodyLarge?.copyWith(
-                        fontWeight: .w600,
-                        decoration: .underline,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
